b2add0514074aefd4aba110f66575856992bec5d
Aryan Agrawal <61811630+AryanKuAg@users.noreply.github.com>
2022-12-15 14:30:53 +0000

    shared faker added and import function added

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,1 @@
-const faker  = require('faker')
-
-let products = ''
-
-for (let i = 0 ; i < 3 ; i++){
-    let item = faker.commerce.productName();
-    products += `<div>${item}</div>`
-}
-
-console.log(products)
+import ('./bootstrap');
